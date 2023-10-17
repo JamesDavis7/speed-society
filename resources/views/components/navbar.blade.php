d852bfe639b20b7063fa212f9681851bc2f264cc
James Davis <james.davis@woodsandwalker.co.uk>
2023-10-17 23:28:21 +0100

    navbar tweak

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,11 +1,15 @@
 <div class="w-full text-black bg-white border-b border-gray-300">
-    <nav class="container flex items-center justify-between h-16 mx-auto text-black">
-        <div class="flex gap-6">
-            <a href="{{ route('meetups.index') }}" wire:navigate>Meetups</a>
-            <a href="{{ route('groups.index') }}" wire:navigate>Groups</a>
+    <nav class="container flex items-center justify-between mx-auto text-black">
+        <div class="flex items-center gap-6">
+            <a href="{{ route('meetups.index') }}" class="py-4" wire:navigate>
+                <span>Meetups</span>
+            </a>
+            <a href="{{ route('groups.index') }}" class="py-4" wire:navigate>
+                <span>Groups</span>
+            </a>        
         </div>
-        <div>
-            <a href="#" wire:navigate>Profile</a>
-        </div>
+        <a href="#" class="py-4" wire:navigate>
+            <span>Profile</span>
+        </a>   
     </nav>
 </div>
